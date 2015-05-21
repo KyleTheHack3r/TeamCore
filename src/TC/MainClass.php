da57db8e33c655c908e546f9b47479f148b4e6be
KyleTheHack3r <thepmteam@hotmail.com>
2015-05-21 23:19:08 +0100

    Added debug message

--- src/TC/MainClass.php
+++ src/TC/MainClass.php
@@ -4,10 +4,13 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use Pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class MainClass extends PluginBase{
 	public function onLoad(){
+                $redTeam = 0;
+                $blueTeam = 0;
 		$this->getLogger()->info("TeamCore has loaded.");
 	}
 	public function onEnable(){
@@ -21,13 +24,25 @@ class MainClass extends PluginBase{
 			case "team":
                     $teams = array('red', 'blue');
                     $team = array_rand($teams);
-      
+                    
+
+                    // for debug or config
+                    if ($teams[$team] == "red"){
+                    $redTeam = $redTeam+1
+                    }
 		    
+
+                    if ($teams[$team] == "blue"){
+                    $blueTeam = $blueTeam+1
+                    }
+
+
                     $sender->sendMessage("-----");
 		    $sender->sendMessage("You are on the " .   $teams[$team] . " team.");
+                    $sender->sendMessage("Debug - Red: " . $redTeam . " Blue: " . $blueTeam . " works");
                     $sender->sendMessage("-----");
 				return true;
-			default:
+	    	default:
 				return false;
 		}
 	}
